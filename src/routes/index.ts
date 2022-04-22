c919ba4fc5d16dcf3a0e74c77ad8539877684e50
alekswheeler <alexskntks@outlook.com>
2022-04-22 16:13:38 -0300

    adicinando o db

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import Router from "express";
 import { congressistaRouter } from "./congressista.routes";
+import { database } from "./database.routes";
 import { hello } from "./teste.routes";
 
 const router = Router();
@@ -7,5 +8,7 @@ const router = Router();
 router.use(congressistaRouter);
 
 router.use(hello);
+
+router.use(database);
 
 export { router };
